56b5d577841d850263d07fb347e1ea222cb98766
Eric Li <e226li@uwaterloo.ca>
2023-09-16 22:10:07 +0100

    Add missing -e to echo

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -14,7 +14,7 @@ do
     for _ in {1..10000}
     do
         int_get=$RANDOM
-        echo "${int_get}\n$((int_get + 123))\n" >> "benchmark-program-1-${i}.cases"
+        echo -e "${int_get}\n$((int_get + 123))\n" >> "benchmark-program-1-${i}.cases"
     done
 done
 
