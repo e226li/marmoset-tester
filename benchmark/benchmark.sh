38932470b80fcda0faff1d11a2a4b1316e76195b
Eric Li <e226li@uwaterloo.ca>
2023-09-16 21:29:44 +0100

    Fix typo, change program count to 100 to reduce time spent on finding files

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -4,17 +4,17 @@ set -e
 mkdir temp
 cd temp
 
-for i in {1..1000}
+for i in {1..100}
 do
     cp "../benchmark-program-1.c" "benchmark-program-1-${i}.c"
 done
 
-for i in {1..1000}
+for i in {1..100}
 do
-    for _ in {1..1000}
+    for _ in {1..10000}
     do
         int_get=$RANDOM
-        echo "${int_get}\n$(int_get + 123)\n" >> "benchmark-program-1-${i}.cases"
+        echo "${int_get}\n$((int_get + 123))\n" >> "benchmark-program-1-${i}.cases"
     done
 done
 
