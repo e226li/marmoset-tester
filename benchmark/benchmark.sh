b801db79eb0398f35e489651525742aed0779c83
Eric Li <e226li@uwaterloo.ca>
2023-09-25 20:03:31 +0100

    Reduce benchmark size

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -4,14 +4,14 @@ set -e
 mkdir temp
 cd temp
 
-for i in {1..10}
+for i in {1..5}
 do
     cp "../benchmark-program-1.c" "benchmark-program-1-${i}.c"
 done
 
-for i in {1..10}
+for i in {1..5}
 do
-    for _ in {1..10000}
+    for _ in {1..1000}
     do
         int_get=$RANDOM
         echo -e "${int_get}\n$((int_get + 123))\n" >> "benchmark-program-1-${i}.cases"
